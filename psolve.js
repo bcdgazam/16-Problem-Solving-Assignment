fd21807a589c1a7f38e25ef0f510be41308ad008
Golam Azam <bcdazam@gmail.com>
2024-04-23 15:27:25 +0600

    basic assignemnt

---
+++ psolve.js
@@ -0,0 +1,10 @@
+//16-Problem-Solving-Assignment
+
+/*
+    1. foot to mile (function name: fooToMile)
+    2. How much woods do I need If I want to make 1 chair, 1 Table and 1 khat? (Function Name: WoodCalculator); Output will be: total woods;
+    3. How many bricks do I need If I want to make a 6th, 15th floor and 22th floor building..peef feet  contains 1k bricks; (Function name: brickCalculator) (1-10: 15foot, 11-20: 12foot, 21-more: 10foot)
+    4. 
+
+*/
+
